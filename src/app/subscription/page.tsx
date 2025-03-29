3633c89ceb6fbcf2fb398145111c00d75ff8fb26
Anderson Tertuliano dos Santos <anderson.tertuliano@apoio.agro.gov.br>
2025-03-29 00:38:26 -0300

    feat: add component page for subscription

--- src/app/subscription/page.tsx
+++ src/app/subscription/page.tsx
@@ -1,9 +1,10 @@
 import { Card, CardContent, CardHeader } from "@/_components/ui/card";
-import { Badge, CheckIcon, XIcon } from "lucide-react";
+import { CheckIcon, XIcon } from "lucide-react";
 import AcquirePlanButton from "./_components/acquire-plan-button";
 import { getCurrentMonthTransactions } from "@/_data/get-current-month-transactions";
 import { auth, clerkClient } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import { Badge } from "@/_components/ui/badge";
 
 export default async function SubscriptionPage() {
   const { userId } = await auth();
@@ -16,9 +17,9 @@ export default async function SubscriptionPage() {
   return (
     <>
       <div className="space-y-6 p-6">
-        <h1 className="text-2xl font-bold">Assinatura</h1>
+        <h1 className="flex justify-center text-2xl font-bold">Assinatura</h1>
 
-        <div className="flex gap-6">
+        <div className="flex justify-center gap-6">
           <Card className="w-[450px]">
             <CardHeader className="border-b border-solid py-8">
               <h2 className="text-center text-2xl font-semibold">
