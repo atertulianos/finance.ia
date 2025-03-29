2d57549539ae820334c178479bf6f884ac66de05
Anderson Tertuliano dos Santos <anderson.tertuliano@apoio.agro.gov.br>
2025-03-29 00:36:30 -0300

    feat: add constants for  dashboard functions

---
+++ src/_constants/dashborad.ts
@@ -0,0 +1,29 @@
+export const MONTH_OPTIONS = [
+  { value: "01", label: "Janeiro" },
+  { value: "02", label: "Fevereiro" },
+  { value: "03", label: "Março" },
+  { value: "04", label: "Abril" },
+  { value: "05", label: "Maio" },
+  { value: "06", label: "Junho" },
+  { value: "07", label: "Julho" },
+  { value: "08", label: "Agosto" },
+  { value: "09", label: "Setembro" },
+  { value: "10", label: "Outubro" },
+  { value: "11", label: "Novembro" },
+  { value: "12", label: "Dezembro" },
+];
+
+// const MONTH_OPTIONS = [
+//   { value: "01", label: "January" },
+//   { value: "02", label: "February" },
+//   { value: "03", label: "March" },
+//   { value: "04", label: "April" },
+//   { value: "05", label: "May" },
+//   { value: "06", label: "June" },
+//   { value: "07", label: "July" },
+//   { value: "08", label: "August" },
+//   { value: "09", label: "September" },
+//   { value: "10", label: "October" },
+//   { value: "11", label: "November" },
+//   { value: "12", label: "December" },
+// ];
